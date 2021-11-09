219d75310b8d583150ac1028a6a99f327d694924
PeterPkn <31990861+PeterPkn@users.noreply.github.com>
2021-11-09 17:43:55 +0100

    Work :)

--- VoicePI/WebUI/voicepiwebui/src/App.js
+++ VoicePI/WebUI/voicepiwebui/src/App.js
@@ -1,15 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
-import MainMenu from './Components/mainMenu';
+import MainMenu from './Pages/mainMenu';
+import Webbrowser from './Pages/webbrowser';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
 
 function App() {
+
+  const rootElement = document.getElementById("root");
+
+
   return (
     <div className="App">
       <header className="App-header">
 
       </header>
       <body className="App-body">
-        <MainMenu/>
+      
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<MainMenu/>} />
+            <Route path="/webbrowser" element={<Webbrowser/>} />
+          </Routes>
+        </BrowserRouter>
+
+        
       </body>
     </div>
   );
