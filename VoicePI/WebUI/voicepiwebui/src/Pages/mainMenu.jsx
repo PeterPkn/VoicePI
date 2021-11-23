a691e036292a21456cfdfcea77648967b5c91cc7
PeterPkn <31990861+PeterPkn@users.noreply.github.com>
2021-11-23 15:36:26 +0100

    sus

--- VoicePI/WebUI/voicepiwebui/src/Pages/mainMenu.jsx
+++ VoicePI/WebUI/voicepiwebui/src/Pages/mainMenu.jsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import "./mainMenu.css";
 import { Typography, Button, Paper } from "@material-ui/core";
 import LanguageIcon  from '@mui/icons-material/Language';
@@ -7,10 +7,21 @@ import KeyboardIcon from '@mui/icons-material/Keyboard';
 import ImageIcon from '@mui/icons-material/Image';
 import AccessibilityIcon from '@mui/icons-material/Accessibility';
 import { useNavigate } from 'react-router';
+import axios from 'axios'
+
 
 
 const MainMenu = () => {
     const [State, setState] = useState(null);
+
+    const [ip, setIP] = useState('None');
+
+    const getData = async () => {
+        const res = await axios.get('https://geolocation-db.com/json/')
+        console.log(res.data);
+        setIP(res.data.IPv4)
+      }
+
 
     const divst = {height:"100%", width:"100%"};
 
@@ -20,12 +31,22 @@ const MainMenu = () => {
 
     let amogus = new Audio("https://srv4.onlymp3.net/download?file=8c61c2c8e24482eea2e2f8baef1863d0251003003&token=pm_iGsxePy1Wxc9WQ8L2xw&expires=1636486376148&s=rKgv3KeW3f6NjV3LtPXlqA");
 
+    
+    useEffect( () => {
+        //passing getData method to the lifecycle method
+        getData()
+    
+      }, [])
+
     const playmogus = () => {
         amogus.play();
     };
 
     return(
         <div class="wrapper">
+            <div style={{position: 'fixed', top: 0, left: 0}}>
+                {ip}
+            </div>
           
             <div class="one">
                 <Paper style={divst}>
