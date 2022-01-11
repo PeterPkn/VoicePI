0a500a8841959aba50f4c21e7c5d827e3f4d6d1a
PeterPkn <31990861+PeterPkn@users.noreply.github.com>
2022-01-11 07:31:51 +0100

    nice style ...

--- VoicePI/Backend/API/__init__.py
+++ VoicePI/Backend/API/__init__.py
@@ -1,15 +1,19 @@
 from flask import Flask
 from flask_restful import Resource, Api
+from flask_cors import CORS, cross_origin
+
 
 app = Flask(__name__)
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['CORS_HEADERS'] = 'Content-Type'
 api = Api(app)
 
 class VoiceAPI(Resource):
     def get(self, request_id):
-        if  (request_id=='1'):
+        if  (request_id=='voice'):
             return {request_id: 'sus'}
-        if  (request_id=='2'):
-            return {request_id: 'sussy'}
+        if  (request_id=='wetter'):
+            return {'wetter': 'Wetter ist schön', 'tage': 'montag...'}
         if  (request_id=='3'):
             return {request_id: 'baka'}
         if  (request_id=='4'):
