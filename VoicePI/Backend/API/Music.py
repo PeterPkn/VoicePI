7bda88fec61d917cf41393b016c2664ddc87a35e
PeterPkn <31990861+PeterPkn@users.noreply.github.com>
2022-01-12 15:31:28 +0100

    UI UPdates and Music integration

---
+++ VoicePI/Backend/API/Music.py
@@ -0,0 +1,65 @@
+from logging import error
+import pafy, vlc, time
+import re, requests, subprocess, urllib.parse, urllib.request
+from bs4 import BeautifulSoup
+
+
+def find_url(name):
+    query_string = urllib.parse.urlencode({"search_query": name})
+    format_url = urllib.request.urlopen("https://www.youtube.com/results?" + query_string)
+
+    search_results = re.findall(r"watch\?v=(\S{11})", format_url.read().decode())
+    clip = "https://www.youtube.com/watch?v=" + "{}".format(search_results[0])
+
+    print(clip)
+    return clip
+
+
+def play_video(url):
+    print("play_video")
+    print(f"url: {url}")
+    video = None
+    while video is None:
+        try:
+            video = pafy.new(url)
+            duration = video.duration
+            print(f"Metadata: \n{video}")
+        except KeyError as error:
+            print("KeyError, Trying again")
+            print(error.with_traceback)
+
+            video = None
+        except URLError as error:
+            print("URL Error, Probaby no Internet Connection")
+            print(error.with_traceback)
+
+    # ftr = [3600, 60, 1]
+    # duration = sum([a * b for a, b in zip(ftr, map(int, duration.split(':')))])
+    print(f"Video: {video}")
+    best = video.getbest()
+    playurl = best.url
+    print(f"Real-URL: {playurl}")
+
+    Instance = vlc.Instance("-I dummy --no-video --aout=alsa --file-logging --logfile=vlc-log.txt --verbose 3")
+    player = Instance.media_player_new()
+    Media = Instance.media_new(playurl)
+    Media.get_mrl()
+
+    player.set_media(Media)
+    player.play()
+
+    time.sleep(10)
+    while player.is_playing() == 1:
+        pass
+        #   print(player.is_playing())
+
+    print("stopping player")
+    player.stop()
+
+
+#urli = find_url("AMOGUS reapster")
+#urli = find_url("Radetzkymarsch")
+#urli = find_url("the Star treks monologue")
+#urli = find_url("ospf lyric")
+
+#play_video(urli)
--- VoicePI/WebUI/voicepiwebui/src/Pages/silentmode.jsx
+++ VoicePI/WebUI/voicepiwebui/src/Pages/silentmode.jsx
@@ -1,3 +1,67 @@
 import * as React from 'react';
-import { useState } from 'react';
-import { Typography, Button, Paper } from "@material-ui/core";
+import { useState, useRef } from 'react';
+import { Typography, Button, Divider, TextField, IconButton } from "@material-ui/core";
+import KeyboardIcon from '@mui/icons-material/Keyboard';
+import SendIcon from '@mui/icons-material/Send';
+import { useNavigate } from 'react-router';
+import MessageBubble from '../component/MessageBubbles';
+
+
+export default function SilentMode(){
+
+    const inputRef = useRef();
+    const iconStyle = {transform: "scale(1.5)", padding:"1%"};
+    const navigate = useNavigate();
+    const [messages, setmessages] = useState([]);
+
+    function sendMessage(){
+        if(inputRef.current.value !== ''){
+            
+
+            fetch(`http://127.0.0.1:5000/silentmode`, {
+                method: "POST",
+                body: JSON.stringify({
+                    msg: inputRef.current.value,
+                }),
+            })
+                .then((response) => response.json())
+                .then((data) => {
+                    setmessages(['VoicePI: ' + data.answ, 'YOU: ' + inputRef.current.value, ...messages]);
+                })
+                .then(()=>inputRef.current.value = '')
+                .catch((error) => {
+                    console.error(error);
+                });
+
+
+        }
+    }
+
+    return(
+
+        <div style={{width:"100%", height:"90vh"}}>
+            <div style={{display:"flex"}}>
+                <Button onClick={()=>{navigate('/')}} style={{minWidth:'100px', minHeight:'50px'}}>Zurück</Button>
+                <KeyboardIcon style={iconStyle}/>
+                <Typography variant='h4'>
+                    Silent Mode
+                </Typography>
+            </div>
+            <Divider></Divider>
+
+            <div style={{maxHeight:'75vh', overflow:'auto', display:'flex', flexDirection:'column-reverse'}}>
+            {messages.map(message=>{
+                if(message.startsWith('YOU:')){
+                    return <MessageBubble message={message} alignLeft={false} />;
+                }
+                return <MessageBubble message={message} alignLeft={true} />;
+            })}  
+            </div>
+
+            <div style={{width:'95%', display:'flex', flexDirection:'row', margin:'20px', position:'absolute', bottom: 0}}>
+                <TextField onKeyUp={(code)=>{if(code.code === 'Enter'){sendMessage();}}} inputRef={inputRef} label='Type your message here.' variant='outlined' style={{flexGrow:'1'}} ></TextField> <IconButton onClick={sendMessage}><SendIcon/></IconButton>
+            </div>
+        </div>
+
+    );
+}
