fcee1390367a4ca66ca183bc57d53353459c4c42
PeterPkn <petsch.pb@gmail.com>
2022-01-19 22:51:00 +0100

    dfg

--- VoicePI/Backend/API/Voice.py
+++ VoicePI/Backend/API/Voice.py
@@ -8,6 +8,9 @@ from subprocess import call
 import requests
 
 from Keyword import Keyword
+import pyttsx3
+
+engine = pyttsx3.init()
 
 r = sr.Recognizer()
 
@@ -23,19 +26,8 @@ logfile = open('voice_tests.txt', 'a')
 
 def speak(speech):
         
-    cmd_beg= 'espeak -vde'
-    cmd_end= ' | aplay /home/pi/Desktop/Text.wav  2>/dev/null' # To play back the stored .wav file and to dump the std errors to /dev/null
-    cmd_out= '--stdout > /home/pi/Desktop/Text.wav ' # To store the voice file
-
-    #text = input("Enter the Text: ")
-    text = speech
-    print(text)
-
-    #Replacing ' ' with '_' to identify words in the text entered
-    text = text.replace(' ', '_')
-
-    #Calls the Espeak TTS Engine to read aloud a Text
-    call([cmd_beg+cmd_out+text+cmd_end], shell=True)
+    engine.say(speech)
+    engine.runAndWait()
 
 def handle_phrase(self, audio1):
     try:
