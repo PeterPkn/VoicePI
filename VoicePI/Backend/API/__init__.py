4d2b5c16d7bf7e1c68ce444566f7ab79e7ada70c
PeterPkn <31990861+PeterPkn@users.noreply.github.com>
2022-01-25 15:19:35 +0100

    sussssssu

--- VoicePI/Backend/API/__init__.py
+++ VoicePI/Backend/API/__init__.py
@@ -65,8 +65,8 @@ def wetter_action(a_string):
     if(a_string.replace("wetter", "").replace("weather", "").strip() is not ""):
         city = a_string
     wth = weather(city)
-    speak(f"Das wetter in {city} ist {wth['weather']}, und es hat {wth['temperature']}  Grad")
-    return jsonify({'answ': f"Das wetter is {wth['weather']}, und es hat {wth['temperature']}  Grad"})
+    speak(f"Das Wetter in {city} ist {wth['weather']}, und es hat {wth['temperature']}  Grad")
+    return jsonify({'answ': f"Das Wetter in {city} ist {wth['weather']}, und es hat {wth['temperature']}  Grad"})
 
 Keyword('play',play_action,1 )
 Keyword('spiele',play_action,1 )
